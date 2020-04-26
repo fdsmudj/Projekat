e6c54c3e27c363544cdee577b12cea9b8628123d
fdsmudj <63963638+fdsmudj@users.noreply.github.com>
2020-04-26 13:39:01 +0200

    apdejt

--- projekat/gamemode.hpp
+++ projekat/gamemode.hpp
@@ -1,11 +1,16 @@
 #ifndef GAMEMODE_HPP_INCLUDED
 #define GAMEMODE_HPP_INCLUDED
-
+#include "mapa.hpp"
 class Gamemode
 {
 protected:
     int numOfPlayers;
-    string mapName;
+    Mapa mapName;
+public:
+    Gamemode(int numOfPlayers1, string name1, int rarity1):mapName(string name1, int rarity1)
+    {
+
+    }
 };
 
 #endif // GAMEMODE_HPP_INCLUDED
