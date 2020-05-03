7f71e31976b285c34e29714271609b10ce18d065
fdsmudj <63963638+fdsmudj@users.noreply.github.com>
2020-05-03 14:26:31 +0200

    Metoda za ability u Characteru

--- projekat/character.hpp
+++ projekat/character.hpp
@@ -1,16 +1,16 @@
 #ifndef CHARACTER_HPP_INCLUDED
 #define CHARACTER_HPP_INCLUDED
-
+enum plN{Bangalore=1, Octane, Pathfinder, Revenant, Lifeline};
 class Character
 {
 protected:
-    string playerName;
+    plN playerName;
     string ability;
     bool cooldown;
     int health;
     int armour;
 public:
-    Character(string playerName1, string ability1, bool cooldown1, int health1, int armour1)
+    Character(plN playerName1, string ability1, bool cooldown1, int health1, int armour1)
     {
         playerName=playerName1;
         ability=ability1;
@@ -22,6 +22,29 @@ public:
     void setArmour(int armor) {armour=armor;}
     int getHealth(){return health;}
     int getArmour(){return armour;}
+    void metodaCh1()
+    {
+        if(playerName==1)
+        {
+            ability='Rolling Thunder';
+        }
+        if(playerName==2)
+        {
+            ability='Launch Pad';
+        }
+        if(playerName==3)
+        {
+            ability='Zipline';
+        }
+        if(playerName==4)
+        {
+            ability='Care Package';
+        }
+        if(playerName==5)
+        {
+            ability='Death Totem';
+        }
+    }
 
 };
 
